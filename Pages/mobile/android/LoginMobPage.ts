79734b68a2ccc13a13b05bd72d5b2b94e84ff4d2
gaurangsolanki <gaurang.solanki@bhavitech.com>
2025-10-06 17:42:17 +0530

    update new wait

--- Pages/mobile/android/LoginMobPage.ts
+++ Pages/mobile/android/LoginMobPage.ts
@@ -35,7 +35,7 @@ class LoginActionsAndroid extends Actions {
         await driver.execute('mobile: activateApp', {
           appId: 'ie.jemstone.ronspot',
         });
-        await browser.pause(50000);
+        await driver.pause(50000);
         const loginBtn = await (await Locatore.loginBtn).isDisplayed().catch(() => false);
         let user: any = null;
         if (loginBtn) {
